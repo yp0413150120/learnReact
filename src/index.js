ee5867203ab9e2458c18c476c7ffb39e08f16d23
沈建斌 <shenjianb@servyou.com.cn>
2022-08-18 21:29:35 +0800

    feat: 学习react源码--didact

--- src/index.js
+++ src/index.js
@@ -1,17 +1,149 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+/** @jsxRuntime classic */
+import logo from './logo.svg';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+
+function createElement(type, props, ...children) {
+  return {
+    type,
+    props: {
+      ...props,
+      children: children.map(item => typeof item === 'object' ? item : createTextElement(item))
+    }
+  }
+}
+
+function createTextElement(text) {
+  return {
+    type: 'TEXT_ELEMENT',
+    props: {
+      nodeValue: text,
+      children: []
+    }
+  }
+}
+
+let nextUnitOfWork = null;
+
+function createDom (fiber) {
+  // 如果没有type，说明是根fiber
+  if (!fiber.type) {
+    return;
+  }
+  const dom = fiber.type === 'TEXT_ELEMENT'
+    ? document.createTextNode('')
+    : document.createElement(fiber.type);
+
+  const isProperty = (key) => key !== 'children';
+
+  Object.keys(fiber.props).filter(isProperty).forEach(key => {
+    dom[key] = fiber.props[key]
+  })
+  fiber.dom.appendChild(dom);
+  return dom;
+}
+
+
+function createRoot(container) {
+  return {
+    render: function (element) {
+      nextUnitOfWork = {
+        dom: container,
+        props: {
+          children: [element],
+        },
+      }
+      requestIdleCallback(workLoop)
+    }
+  }
+}
+
+/** 寻找下一个fiber */
+function findNextFiber(fiber) {
+  if (fiber.child) return fiber.child;
+  if (fiber.sibling) return fiber.sibling;
+  while(fiber.parent) {
+    fiber = fiber.parent;
+    if (fiber.sibling) return fiber.sibling;
+  }
+  return null;
+}
+
+/** 渲染当前fiber */
+function performUnitOfWork (fiber) {
+  const dom = createDom(fiber);
+  const unitChildren = fiber.props.children;
+  if (unitChildren.length) {
+    unitChildren.reduce((preValue, child, idx) => {
+      const newFiber = {
+        dom: dom || fiber.dom,
+        parent: fiber,
+        ...child
+      }
+      if (idx === 0) {
+        fiber.child = newFiber;
+      }
+      if (preValue) {
+        preValue.sibling = newFiber;
+      }
+      return newFiber;
+    }, undefined)
+  }
+  return findNextFiber(fiber);
+}
+
+function workLoop(deadline) {
+  let shouldYield = false
+  while (nextUnitOfWork && !shouldYield) {
+    nextUnitOfWork = performUnitOfWork(
+      nextUnitOfWork
+    )
+    shouldYield = deadline.timeRemaining() < 1
+  }
+  requestIdleCallback(workLoop)
+}
+
+const Didact = {
+  createElement,
+  createRoot
+};
+
+/** @jsx Didact.createElement */
+const App = (
+  <div className="App">
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>
+        Edit <code>src/App.js</code> and save to reload.
+      </p>
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn React11
+      </a>
+    </header>
+  </div>
 );
+
+const root = Didact.createRoot(document.getElementById('root'));
+root.render(App);
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals(console.log);
