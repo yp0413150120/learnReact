34d5b1420d5c34c943b0c42d2871422448af8aa8
沈建斌 <shenjianb@servyou.com.cn>
2022-08-19 18:35:24 +0800

    feat: 完善didact

---
+++ src/didact.js
@@ -0,0 +1,214 @@
+function createElement(type, props, ...children) {
+  return {
+    type,
+    props: {
+      ...props,
+      children: children.map(item => typeof item === 'object' ? item : createTextElement(item))
+    }
+  }
+}
+
+function createTextElement(text) {
+  return {
+    type: 'TEXT_ELEMENT',
+    props: {
+      nodeValue: text,
+      children: []
+    }
+  }
+}
+
+const isProperty = (key) => key !== 'children';
+const isGone = (preProps, nextProps) => {
+  return (key) => (key in preProps) && !(key in nextProps)
+}
+const isNew = (preProps, nextProps) => {
+  return (key) => nextProps[key] !== preProps[key]
+}
+const isEvent = (name) => name.startWith('on');
+
+
+function createDom(fiber) {
+  const dom = fiber.type === 'TEXT_ELEMENT'
+  ? document.createTextNode('')
+  : document.createElement(fiber.type);
+  
+  const isProperty = (key) => key !== 'children';
+  
+  Object.keys(fiber.props).filter(isProperty).forEach(key => {
+    dom[key] = fiber.props[key]
+  })
+  return dom;
+}
+
+function updateDom(dom, prevProps, nextProps) {
+  // 旧属性移除，对于事件需要特殊处理
+  Object.keys(prevProps)
+    .filter(isEvent)
+    .filter(key => !(key in nextProps) || isNew(prevProps, nextProps)(key))
+    .forEach(key => dom.removeEventListener(key.toLocaleLowerCase().slice(2), prevProps[key]));
+  Object.keys(prevProps).filter(isProperty).filter(isGone(prevProps, nextProps)).forEach(key => {
+    dom[key] = undefined;
+  })
+  // 新属性添加，对于事件需要特殊处理
+  Object.keys(nextProps)
+    .filter(isEvent)
+    .filter(isNew(prevProps, nextProps))
+    .forEach(key => dom.addEventLisntener(key.toLocaleLowerCase().slice(2), nextProps[key]));
+  Object.keys(nextProps).filter(isProperty).filter(isNew(prevProps, nextProps)).forEach(key => {
+      dom[key] = nextProps[key];
+  })
+}
+
+let nextUnitOfWork = null;
+let wipRoot = null;
+let currentRoot = null;
+let deletions = [];
+
+function createRoot(container) {
+  return {
+    render: function (element) {
+      wipRoot = {
+        dom: container,
+        props: {
+          children: [element],
+        },
+        alternate: currentRoot,
+      }
+      deletions = []
+      nextUnitOfWork = wipRoot;
+      requestIdleCallback(workLoop)
+    }
+  }
+}
+
+
+
+function commitRoot() {
+  deletions.forEach(commitWork);
+  commitWork(wipRoot.child);
+  currentRoot = wipRoot;
+  wipRoot = null;
+}
+
+function commitWork(fiber) {
+  if (!fiber) return;
+  const domParent = fiber.parent.dom;
+  // 如果是要删除的fiber，卸载
+  if (fiber.effectTags === 'DELETION') {
+    domParent.removeChild(fiber.dom);
+  }
+  else if (fiber.effectTags === 'PLACEMENT' && fiber.dom) {
+    // 如果是新增的fiber
+    domParent?.append(fiber.dom);
+    commitWork(fiber.child);
+    commitWork(fiber.sibling);
+  } else if (fiber.effectTags === 'UPDATE') {
+    // 如果只是props更新
+    updateDom(fiber.dom, fiber.alternate.props, fiber.props);
+  }
+}
+
+/** 寻找下一个fiber */
+function findNextFiber(fiber) {
+  if (fiber.child) return fiber.child;
+  if (fiber.sibling) return fiber.sibling;
+  while (fiber.parent) {
+    fiber = fiber.parent;
+    if (fiber.sibling) return fiber.sibling;
+  }
+  return null;
+}
+
+/** 渲染当前fiber */
+function performUnitOfWork(fiber) {
+  if (!fiber.dom) {
+    fiber.dom = createDom(fiber);
+  }
+  reconcileChildren(fiber, fiber.props.children);
+  return findNextFiber(fiber);
+}
+
+function reconcileChildren(wibFiber, elements) {
+  if (elements.length) {
+    let oldFiber = wibFiber.alternate && wibFiber.alternate.child;
+    let index = 0;
+    let preFiber;
+    while(index < elements.length || oldFiber) {
+      const element = elements[index];
+      let newFiber
+      const sameType = element && oldFiber && element.type === oldFiber.type;
+      // 当前元素和旧fiber一样
+      if (element && oldFiber && sameType) {
+        newFiber = {
+          dom: undefined,
+          parent: wibFiber,
+          type: element.type,
+          props: element.props,
+          alternate: oldFiber,
+          effectTags: 'UPDATE',
+        }
+      }
+      // 当前元素和旧fiber不一样
+      if (element && !sameType) {
+        newFiber = {
+          dom: undefined,
+          parent: wibFiber,
+          type: element.type,
+          props: element.props,
+          alternate: null,
+          effectTags: 'PLACEMENT',
+        }
+      }
+      if (oldFiber && !sameType) {
+        oldFiber.effectTags = 'DELETION';
+        deletions.push(oldFiber);
+      }
+
+      if (index === 0) {
+        wibFiber.child = newFiber;
+      }
+      if (preFiber) {
+        preFiber.sibling = newFiber;
+      }
+      if (oldFiber) {
+        oldFiber = oldFiber.sibling;
+      }
+      preFiber = newFiber;
+      index ++;
+    }
+    // elements.reduce((preValue, child, idx) => {
+    //   const newFiber = {
+    //     dom: undefined,
+    //     parent: wibFiber,
+    //     ...child
+    //   }
+    //   if (idx === 0) {
+    //     wibFiber.child = newFiber;
+    //   }
+    //   if (preValue) {
+    //     preValue.sibling = newFiber;
+    //   }
+    //   return newFiber;
+    // }, undefined)
+  }
+}
+
+function workLoop(deadline) {
+  let shouldYield = false
+  while (nextUnitOfWork && !shouldYield) {
+    nextUnitOfWork = performUnitOfWork(
+      nextUnitOfWork
+    )
+    shouldYield = deadline.timeRemaining() < 1
+  }
+  if (!nextUnitOfWork && wipRoot) {
+    commitRoot();
+  }
+  requestIdleCallback(workLoop)
+}
+
+export default {
+  createElement,
+  createRoot
+};
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 /** @jsxRuntime classic */
 import logo from './logo.svg';
 import './index.css';
+import Didact from './didact';
 // import React from 'react';
 // import ReactDOM from 'react-dom/client';
 // import App from './App';
@@ -12,123 +13,6 @@ import './index.css';
 //     <App />
 //   </React.StrictMode>
 // );
-
-
-function createElement(type, props, ...children) {
-  return {
-    type,
-    props: {
-      ...props,
-      children: children.map(item => typeof item === 'object' ? item : createTextElement(item))
-    }
-  }
-}
-
-function createTextElement(text) {
-  return {
-    type: 'TEXT_ELEMENT',
-    props: {
-      nodeValue: text,
-      children: []
-    }
-  }
-}
-
-let nextUnitOfWork = null;
-let wipRoot = null;
-
-function createDom(fiber) {
-  // 如果没有type，说明是根fiber
-  if (!fiber.type) {
-    return;
-  }
-  const dom = fiber.type === 'TEXT_ELEMENT'
-    ? document.createTextNode('')
-    : document.createElement(fiber.type);
-
-  const isProperty = (key) => key !== 'children';
-
-  Object.keys(fiber.props).filter(isProperty).forEach(key => {
-    dom[key] = fiber.props[key]
-  })
-  return dom;
-}
-
-
-function createRoot(container) {
-  return {
-    render: function (element) {
-      nextUnitOfWork = {
-        dom: container,
-        props: {
-          children: [element],
-        },
-      }
-      requestIdleCallback(workLoop)
-    }
-  }
-}
-
-/** 寻找下一个fiber */
-function findNextFiber(fiber) {
-  if (fiber.child) return fiber.child;
-  if (fiber.sibling) return fiber.sibling;
-  while (fiber.parent) {
-    fiber = fiber.parent;
-    if (fiber.sibling) return fiber.sibling;
-  }
-  return null;
-}
-
-/** 渲染当前fiber */
-function performUnitOfWork(fiber) {
-  if (!fiber.dom) {
-    fiber.dom = createDom(fiber);
-  }
-  if (fiber.parent) {
-    fiber.parent.dom.append(fiber.dom);
-  }
-  const unitChildren = fiber.props.children;
-  if (unitChildren.length) {
-    unitChildren.reduce((preValue, child, idx) => {
-      const newFiber = {
-        dom: undefined,
-        parent: fiber,
-        ...child
-      }
-      if (idx === 0) {
-        fiber.child = newFiber;
-      }
-      if (preValue) {
-        preValue.sibling = newFiber;
-      }
-      return newFiber;
-    }, undefined)
-  }
-  return findNextFiber(fiber);
-}
-
-function workLoop(deadline) {
-  let shouldYield = false
-  if (wipRoot) {
-    nextUnitOfWork = wipRoot;
-  }
-  while (nextUnitOfWork && !shouldYield) {
-    nextUnitOfWork = performUnitOfWork(
-      nextUnitOfWork
-    )
-    shouldYield = deadline.timeRemaining() < 1
-  }
-  if (nextUnitOfWork) {
-    wipRoot = nextUnitOfWork;
-  }
-  requestIdleCallback(workLoop)
-}
-
-const Didact = {
-  createElement,
-  createRoot
-};
 
 /** @jsx Didact.createElement */
 const App = (
